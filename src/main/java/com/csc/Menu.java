8397252ee4eafb53f68db1d26d32e61ef061ee05
Ahtisham8 <84299800+Ahtisham8@users.noreply.github.com>
2024-09-10 17:47:01 -0700

    Created the Menu Class
    Menu Class takes in parameters for upper bound, lower, bound, errorMsg, and a prompt. The user is able to enter a value that fits the criteria given.

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -1,3 +1,29 @@
 package com.csc;
 
+import java.util.Scanner;
+
 // Your code goes here!
+class main{
+    public static void main(String[] args)
+    {
+        System.out.println("Java Menu:");
+        menu(500, -500, "Your value is invalid", "Please enter a value");
+        
+    }
+    
+    public static void menu(int upper, int lower, String errorMsg, String prompt)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println(prompt);
+        int num = input.nextInt();
+    
+        while (num > upper || num < lower) {
+            System.out.println(errorMsg);
+            System.out.println(prompt);
+            num = input.nextInt();
+        }
+    
+        System.out.println("The value chosen by the user is " + num);
+    }
+}
