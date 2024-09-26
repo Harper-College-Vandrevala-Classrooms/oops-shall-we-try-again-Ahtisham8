e40d56904d7a4ff0208b258f7be46901fe7034b0
Ahtisham8 <84299800+Ahtisham8@users.noreply.github.com>
2024-09-26 16:22:49 -0700

    Finished the default and exit scenarios
    If the user enters "default" then the default value of 100 is returned. If the user enters "exit" then a statement saying that the function is aborted is displayed.

--- src/main/java/com/csc/Menu.java
+++ src/main/java/com/csc/Menu.java
@@ -7,23 +7,41 @@ class main{
     public static void main(String[] args)
     {
         System.out.println("Java Menu:");
-        menu(500, -500, "Your value is invalid", "Please enter a value");
+        menu(500, -500, "Your value is invalid", "Please enter a value. Enter \"default\" to use the default value of 100. Enter \"exit\" to leave the menu.\r\n");
         
     }
     
     public static void menu(int upper, int lower, String errorMsg, String prompt)
     {
         Scanner input = new Scanner(System.in);
+        String output = "";
         
         System.out.println(prompt);
-        int num = input.nextInt();
-    
-        while (num > upper || num < lower) {
-            System.out.println(errorMsg);
-            System.out.println(prompt);
-            num = input.nextInt();
+        String num = input.nextLine();
+        
+        if (num.equals("default") || num.equals("Default")) {
+
+            System.out.println("The value chosen by the user is " + 100);
         }
-    
-        System.out.println("The value chosen by the user is " + num);
+
+        else if(num.equals("exit")){
+            System.out.println("The program will now be aborted.");
+        }
+        
+        else {
+
+            int castNum = Integer.parseInt(num);
+            
+            while (castNum > upper || castNum < lower) {
+                System.out.println(errorMsg);
+                System.out.println(prompt);
+                castNum = input.nextInt();
+            }
+
+            System.out.println("The value chosen by the user is " + castNum);
+        }
+
+        
+        
     }
 }
